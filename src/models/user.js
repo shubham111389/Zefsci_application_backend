c361fdf7eb4660d2c09c64a5ab908b75bd7df454
shubham annpurne <shubhamannpurne1029@gmail.com>
2025-07-01 18:55:29 +0530

    added auth middleware

--- src/models/user.js
+++ src/models/user.js
@@ -52,6 +52,35 @@ const userSchema = new mongoose.Schema({
     },
    // required: [true, "Phone number is required"],
   },
-});
+
+},
+ {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.getJWT = async function () {
+  const user = this;
+
+  const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
+    expiresIn: "7d",
+  });
+
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+
+  return isPasswordValid;
+};
+
+
 
 module.exports = mongoose.model("User", userSchema);
